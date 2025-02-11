75ca360696e5caa683cad5073c7d2fa6a4f878a8
mshudrak <69989229+mshudrak@users.noreply.github.com>
2025-02-11 15:07:01 -0800

    Update gen_doc.py
    Signed-off-by: mshudrak <69989229+mshudrak@users.noreply.github.com>

--- onnx/defs/gen_doc.py
+++ onnx/defs/gen_doc.py
@@ -5,16 +5,4 @@ def main(args: Args) -> None:
     print("We are executing this file.")
 
 if __name__ == "__main__":
-    if ONNX_ML:
-        main(
-            Args(
-                "Operators-ml.md",
-                "Changelog-ml.md",
-            )
-        )
-    main(
-        Args(
-            "Operators.md",
-            "Changelog.md",
-        )
-    )
+    print("We are executing this file.")
